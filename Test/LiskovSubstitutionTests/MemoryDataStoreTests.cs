3bdbb7e2d67153f87ddb599a7e45f84f64c0f84c
raagamparmar <raagamparmar@gmail.com>
2026-08-27 21:32:49 +0530

    Add basic tests for memory providers.

---
+++ Test/LiskovSubstitutionTests/MemoryDataStoreTests.cs
@@ -0,0 +1,35 @@
+﻿using LiskovSubstitution;
+
+namespace Test.LiskovSubstitutionTests
+{
+    public class MemoryDataStoreTests
+    {
+        /// <summary>
+        /// Tests that the Read method returns the correct value after saving a key-value pair in the MemoryDataStore.
+        /// </summary>
+        [Fact]
+        public void ReadAfterSaveIsCorrect_01()
+        {
+            IDataStore store = new MemoryDataStore();
+
+            store.Save("nat", "Type");
+
+            string? value = store.Read("nat");
+
+            Assert.Equal("Type", value);
+        }
+
+        /// <summary>
+        /// Tests that the Read method returns null when attempting to read a key that has not been saved in the MemoryDataStore.
+        /// </summary>
+        [Fact]
+        public void ReadBeforeSaveIsNull_01()
+        {
+            IDataStore store = new MemoryDataStore(); 
+
+            string? value = store.Read("nat");
+
+            Assert.Null(value);
+        }
+    }
+}
