abd90d59e3826a72cd8cec4963a82798e0a3728e
raagamparmar <raagamparmar@gmail.com>
2026-08-27 21:31:42 +0530

    Add memory-based data storage provider.

---
+++ LiskovSubstitution/MemoryDataStore.cs
@@ -0,0 +1,17 @@
+﻿namespace LiskovSubstitution
+{
+    public sealed class MemoryDataStore : IDataStore
+    {
+        private readonly Dictionary<string, string> _data = [];
+
+        public void Save(string key, string value)
+        {
+            _data[key] = value;
+        }
+
+        public string? Read(string key)
+        {
+            return _data.TryGetValue(key, out var value) ? value : null;
+        }
+    }
+}
