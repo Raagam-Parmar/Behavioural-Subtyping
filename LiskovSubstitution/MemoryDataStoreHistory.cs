83f9facc7d4485114493725390436c1ee6a25776
raagamparmar <raagamparmar@gmail.com>
2026-08-28 18:05:19 +0530

    Add memory-based data storage provider for history tracking.

---
+++ LiskovSubstitution/MemoryDataStoreHistory.cs
@@ -0,0 +1,39 @@
+﻿namespace LiskovSubstitution
+{
+    public sealed class MemoryDataStoreHistory : IDataStoreHistory
+    {
+        /// <summary>
+        /// A dictionary to hold the key-value pairs with history tracking in memory.
+        /// </summary>
+        private readonly Dictionary<string, Stack<string>> _data = [];
+        
+        /// <inheritdoc/>
+        public void Save(string key, string value)
+        {
+            if (!_data.ContainsKey(key))
+            {
+                _data[key] = new Stack<string>();
+            }
+            _data[key].Push(value);
+        }
+
+        /// <inheritdoc/>
+        public string? Read(string key)
+        {
+            return _data.TryGetValue(key, out var value) ? value.Last() : null;
+        }
+
+        /// <inheritdoc/>
+        public void Revert(string key)
+        {
+            if (_data.ContainsKey(key) && _data[key].Count > 1)
+            {
+                _data[key].Pop();
+            }
+            else if (_data.ContainsKey(key) && _data[key].Count == 1)
+            {
+                _data.Remove(key);
+            }
+        }
+    }
+}
