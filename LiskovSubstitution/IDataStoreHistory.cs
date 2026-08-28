be096ff1c9e6051cdc7a00e46762de35b9c68ea2
raagamparmar <raagamparmar@gmail.com>
2026-08-28 18:04:31 +0530

    Add contract for data storage providers with history tracking.

---
+++ LiskovSubstitution/IDataStoreHistory.cs
@@ -0,0 +1,16 @@
+﻿namespace LiskovSubstitution
+{
+    /// <summary>
+    /// A contract for data storage providers which support history tracking.
+    /// </summary>
+    public interface IDataStoreHistory : IDataStore
+    {
+        /// <summary>
+        /// Reverts the value associated with the specified key to its previous state.
+        /// If the key is not found, it does nothing.
+        /// If there is only one value in the history for the key, it removes the key from the data store.
+        /// </summary>
+        /// <param name="key">The key for the data to revert.</param>
+        void Revert(string key);
+    }
+}
