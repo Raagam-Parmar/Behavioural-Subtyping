bcd1a23fe5ce846e43bf8e758c39ac1cbb4d8a47
raagamparmar <raagamparmar@gmail.com>
2026-08-29 01:37:28 +0530

    Add collision test for FileDataStore.

--- Test/LiskovSubstitutionTests/FileDataStoreTests.cs
+++ Test/LiskovSubstitutionTests/FileDataStoreTests.cs
@@ -8,4 +8,30 @@ public class FileDataStoreTests : DataStoreContractTests<FileDataStore>
     {
         return new FileDataStore();
     }
+
+    /// <summary>
+    /// Verifies two keys which contain invalid filepath characters at the same location, and are equal at
+    /// all other indices, will collide in the FileDataStore. This is a known limitation of the FileDataStore implementation.
+    /// </summary>
+    [Fact]
+    public void CollidingKeysInFileDataStore()
+    {
+        char[] invalidChars = System.IO.Path.GetInvalidFileNameChars();
+
+        FileDataStore store = CreateStore();
+
+        foreach (char invalidChar in invalidChars)
+        {
+            string key1 = $"key{invalidChar}";
+            store.Save(key1, "value1");
+            Assert.Equal("value1", store.Read(key1));
+
+            string key2 = $"key{invalidChar}";
+            store.Save(key2, "value2");
+            Assert.Equal("value2", store.Read(key2));
+
+            // The two keys collide and return the same value
+            Assert.Equal("value2", store.Read(key1));
+        }
+    }
 }
