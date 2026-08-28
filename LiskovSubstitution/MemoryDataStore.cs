7336672ed94bd8223f5eafa0b852c2ca962f292e
raagamparmar <raagamparmar@gmail.com>
2026-08-28 09:35:14 +0530

    Add docstrings.

--- LiskovSubstitution/MemoryDataStore.cs
+++ LiskovSubstitution/MemoryDataStore.cs
@@ -2,13 +2,18 @@
 {
     public sealed class MemoryDataStore : IDataStore
     {
+        /// <summary>
+        /// A dictionary to hold the key-value pairs in memory.
+        /// </summary>
         private readonly Dictionary<string, string> _data = [];
-
+        
+        /// <inheritdoc/>
         public void Save(string key, string value)
         {
             _data[key] = value;
         }
 
+        /// <inheritdoc/>
         public string? Read(string key)
         {
             return _data.TryGetValue(key, out var value) ? value : null;
