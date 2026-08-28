33170b6d1410e0467efe2d4668f140ee10543a7a
raagamparmar <raagamparmar@gmail.com>
2026-08-28 17:08:13 +0530

    Add MemoryStore class which stores only integers, as a counter-example of behavioural subtyping.

---
+++ LiskovSubstitution/IntMemoryDataStore.cs
@@ -0,0 +1,33 @@
+﻿namespace LiskovSubstitution
+{
+    public class IntMemoryDataStore : MemoryDataStore, IDataStore
+    {
+        /// <summary>
+        /// Sanitizes the input data to ensure it is an integer.
+        /// </summary>
+        /// <param name="data">The input data to sanitize.</param>
+        /// <returns>The sanitized integer as a string.</returns>
+        /// <exception cref="ArgumentException">Thrown when the input data is not a valid integer.</exception>
+        private string Sanitize(string data)
+        {
+            bool parseSuccess = int.TryParse(data, out var value);
+
+            if (!parseSuccess) {
+                throw new ArgumentException("Value must be an integer.", nameof(data));
+            }
+
+            return value.ToString();
+        }
+
+        /// <summary>
+        /// Saves the key-value pair to the data store after sanitizing the value to ensure it is an integer.
+        /// </summary>
+        /// <param name="key">The key for the data store.</param>
+        /// <param name="value">The value to save, which will be sanitized to an integer.</param>
+        public override void Save(string key, string value)
+        {
+            var sanitizedValue = Sanitize(value);
+            base.Save(key, sanitizedValue);
+        }
+    }
+}
