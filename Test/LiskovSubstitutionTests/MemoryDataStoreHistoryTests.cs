8adfdce10511ee27ba6a4f6866f996c23b508217
raagamparmar <raagamparmar@gmail.com>
2026-08-28 18:06:00 +0530

    Add test for history-tracking memory storage providers.

---
+++ Test/LiskovSubstitutionTests/MemoryDataStoreHistoryTests.cs
@@ -0,0 +1,37 @@
+﻿using LiskovSubstitution;
+
+namespace Test.LiskovSubstitutionTests
+{
+    public class MemoryDataStoreHistoryTests : DataStoreContractTests<MemoryDataStoreHistory>
+    {
+        protected override MemoryDataStoreHistory CreateStore()
+        {
+            return new MemoryDataStoreHistory();
+        }
+
+        /// <summary>
+        /// Tests the Revert method of the MemoryDataStoreHistory class.
+        /// </summary>
+        [Fact]
+        public void RevertTest()
+        {
+            var store = CreateStore();
+
+            // Assuming that the data store is being used for type-checking
+            store.Save("x", "int");
+            store.Save("x", "string");
+            store.Save("x", "float");
+
+            Assert.Equal("float", store.Read("x"));
+
+            store.Revert("x");
+            Assert.Equal("string", store.Read("x"));
+
+            store.Revert("x");
+            Assert.Equal("int", store.Read("x"));
+
+            store.Revert("x");
+            Assert.Null(store.Read("x"));
+        }
+    }
+}
