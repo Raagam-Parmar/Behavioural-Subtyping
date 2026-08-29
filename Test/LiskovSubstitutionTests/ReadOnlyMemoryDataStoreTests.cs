70f213339b029eef0421b9758da1af40115ab702
raagamparmar <raagamparmar@gmail.com>
2026-08-29 10:55:26 +0530

    Add test for ReadOnlyMemoryDataStore.

---
+++ Test/LiskovSubstitutionTests/ReadOnlyMemoryDataStoreTests.cs
@@ -0,0 +1,37 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using LiskovSubstitution;
+
+namespace Test.LiskovSubstitutionTests;
+
+public class ReadOnlyMemoryDataStoreTests
+{
+    /// <summary>
+    /// Tests that the Save method of ReadOnlyMemoryDataStore throws an InvalidOperationException when called.
+    /// </summary>
+    [Fact]
+    public void SaveThrowsInvalidOperationException()
+    {
+        ReadOnlyMemoryDataStore store = new ReadOnlyMemoryDataStore();
+
+        Assert.Throws<InvalidOperationException>(() => store.Save("NewKey", "NewValue"));
+    }
+
+    /// <summary>
+    /// Tests that the Read method of ReadOnlyMemoryDataStore does not throw an exception and returns the expected value for existing keys.
+    /// </summary>
+    [Fact]
+    public void ReadDoesNotThrow()
+    {
+        ReadOnlyMemoryDataStore store = new ReadOnlyMemoryDataStore();
+
+        for (int i = 0; i < 10; i++)
+        {
+            string? value = store.Read($"RO_Key_{i}");
+
+            Assert.Equal($"RO_Value_{i}", value);
+        }
+    }
+}
